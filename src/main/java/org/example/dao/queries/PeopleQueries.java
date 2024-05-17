7667afe3e3dd2bc6f3aa67836aa5aa6bdfa4a6bb
swe2mohamed <swe.mohamed1990@gmail.com>
2024-05-17 06:13:46 +0200

    third  version: - Finish ToDoItemQueries and PeopleQueries.
    Assignment 'Not Finish' yet

--- src/main/java/org/example/dao/queries/PeopleQueries.java
+++ src/main/java/org/example/dao/queries/PeopleQueries.java
@@ -2,10 +2,10 @@ package org.example.dao.queries;
 
 public class PeopleQueries {
 
-    public static final String CREATE = "";
-    public static final String UPDATE= "";
-    public static final String DELETE = "";
-    public static final String FIND_ALL = "";
-    public static final String FIND_BY_ID = "";
-    public static final String FIND_BY_NAME = "";
+    public static final String CREATE = "INSERT INTO person (first_name, last_name) VALUES (?, ?)";
+    public static final String UPDATE = "UPDATE person SET first_name = ?, last_name = ? WHERE person_id = ?";
+    public static final String DELETE = "DELETE FROM person WHERE person_id = ?";
+    public static final String FIND_ALL = "SELECT * FROM person";
+    public static final String FIND_BY_ID = "SELECT * FROM person WHERE person_id = ?";
+    public static final String FIND_BY_NAME = "SELECT * FROM person WHERE first_name = ? AND last_name = ?";
 }
--- src/main/java/org/example/dao/queries/ToDoItemQueries.java
+++ src/main/java/org/example/dao/queries/ToDoItemQueries.java
@@ -1,13 +1,13 @@
 package org.example.dao.queries;
 
 public class ToDoItemQueries {
-    public static final String CREATE = "";
-    public static final String UPDATE = "";
-    public static final String DELETE = "";
-    public static final String FIND_ALL = "";
-    public static final String FIND_BY_ID = "";
-    public static final String FIND_BY_DONE_STATUS = "";
-    public static final String FIND_BY_ASSIGNEE_ID = "";
-    public static final String FIND_BY_ASSIGNEE_PEOPLE = "";
-    public static final String FIND_BY_UNASSIGNED = "";
+    public static final String CREATE = "INSERT INTO todo_item (title, description, deadline, done, assignee_id) VALUES (?, ?, ?, ?, ?)";
+    public static final String UPDATE = "UPDATE todo_item SET title = ?, description = ?, deadline = ?, done = ?, assignee_id = ? WHERE todo_id = ?";
+    public static final String DELETE = "DELETE FROM todo_item WHERE todo_id = ?";
+    public static final String FIND_ALL = "SELECT * FROM todo_item";
+    public static final String FIND_BY_ID = "SELECT * FROM todo_item WHERE todo_id = ?";
+    public static final String FIND_BY_DONE_STATUS = "SELECT * FROM todo_item WHERE done = ?";
+    public static final String FIND_BY_ASSIGNEE_ID = "SELECT * FROM todo_item WHERE assignee_id = ?";
+    public static final String FIND_BY_ASSIGNEE_PEOPLE = "SELECT * FROM todo_item INNER JOIN person ON todo_item.assignee_id = person.person_id";
+    public static final String FIND_BY_UNASSIGNED = "SELECT * FROM todo_item WHERE assignee_id IS NULL";
 }
